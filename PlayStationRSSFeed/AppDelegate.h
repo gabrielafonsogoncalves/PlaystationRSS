dc541fecdb10f878d8a0b3e56231207f8936ce83
Gabriel Afonso <gabriel.afonso@educatablet.com>
2015-01-29 01:08:18 -0430

    Initial Commit

---
+++ PlayStationRSSFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayStationRSSFeed
+//
+//  Created by Gabriel Afonso on 1/29/15.
+//  Copyright (c) 2015 ETC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayStationRSSFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayStationRSSFeed
+//
+//  Created by Gabriel Afonso on 1/29/15.
+//  Copyright (c) 2015 ETC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
